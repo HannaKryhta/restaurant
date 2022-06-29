ca6ced2ba3060aea8f12cc0693fc0bee62e49ab1
ANNA <anna.Kryhta @gmail.com>
2022-06-29 21:56:50 +0300

    animation

---
+++ script.js
@@ -0,0 +1,6 @@
+gsap.to(".one",{delay:1, duration:2, x:-400, ease: "power2.inOut", opacity:0})
+gsap.from(".two",{delay:1.2, duration:2, x:-400, ease: "power2.inOut", opacity:0})
+gsap.from(".header",{delay:2, duration:2.5, y:100,  opacity:0})
+gsap.from(".btn",{delay:2, duration:4, rotation:720,  opacity:0})
+
+
